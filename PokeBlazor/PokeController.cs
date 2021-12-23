dcbbd0ec6dac4387e2de17be8dd9142bcabc46fa
Trevor Stuart <stuart.trev@gmail.com>
2021-12-23 08:43:25 -0500

    initial

---
+++ PokeBlazor/PokeController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokeBlazor
+{
+    public class PokeController : IPokeController
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokeController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Pokemon> GetPokemon(string name)
+        {
+            var result = JsonConvert.DeserializeObject<Pokemon>(await _httpClient.GetStringAsync($"pokemon/{name}"));
+
+            if (result.Name.Length == 0)
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PokeBlazor/Pokemon.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeBlazor
+{
+    public class Pokemon
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("base_experience")]
+        public int Experience { get; set; }
+        [JsonProperty("order")]
+        public int Order { get; set; }
+    }
+}
